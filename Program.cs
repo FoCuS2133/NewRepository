9f2d73a681e13e29123803bf3bb228b615bd1057
FoCuS2133 <Andreyka.lapshin.2002@mail.ru>
2024-09-21 19:23:47 +0300

    Добавлен 1й файл с кодом

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+using System.Threading.Channels;
+using Triangle;
+
+ForTriangle one = new();
+Console.WriteLine("Введите стороны треугольника");
+string a=Console.ReadLine();
+string b=Console.ReadLine();    
+string c=Console.ReadLine();
+uint A=uint.Parse(a);
+uint B=uint.Parse(b);
+uint C=uint.Parse(c);
+
+
+one.Perimeter();
+one.Area();
